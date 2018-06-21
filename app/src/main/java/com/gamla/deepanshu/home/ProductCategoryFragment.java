8f34380434ded8a870d698635d705316f3eb73f6
deepu1987 <deepanshu.misra@gmail.com>
2018-06-22 00:53:29 +0530

    third Commit

--- app/src/main/java/com/gamla/deepanshu/home/ProductCategoryFragment.java
+++ app/src/main/java/com/gamla/deepanshu/home/ProductCategoryFragment.java
@@ -85,7 +85,7 @@ public class ProductCategoryFragment extends Fragment implements OnCategoryItemC
         View v = inflater.inflate(R.layout.product_category_fragment, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Home");
         _context = getActivity();
-        // coment
+        // coment new new
 
      /*   mAdView = (AdView) v.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
